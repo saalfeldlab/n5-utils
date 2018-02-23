saalfeldlab/n5-utils

commit d6e7d3afc19e6a2425cda8002b21afddcff0ccc9

do not copy/ overwrite dataset attributes as general attributes

src/main/java/org/saalfeldlab/Copy.java
@@ -20,8 +20,10 @@ import java.io.IOException;
 import java.io.Serializable;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -241,6 +243,8 @@ public class Copy {
 
 	protected <T extends NativeType<T>> void copyDataset(final String datasetName) throws IOException, InterruptedException, ExecutionException {
 
+		System.out.println(datasetName);
+
 		final DatasetAttributes datasetAttributes = n5Reader.getDatasetAttributes(datasetName);
 
 		final RandomAccessibleInterval<T> dataset;
@@ -276,19 +280,31 @@ public class Copy {
 	protected void copyAttributes(final String groupName)
 			throws IOException {
 
+		System.out.println("  attributes:");
+
 		final Map<String, Class<?>> attributes = n5Reader.listAttributes(groupName);
+		final DatasetAttributes datasetAttributes = n5Reader.getDatasetAttributes(groupName);
+		final Set<String> datasetAttributeKeys = datasetAttributes == null ? new HashSet<>() : datasetAttributes.asMap().keySet();
+
 		attributes.forEach((key, clazz) -> {
-			try {
-				n5Writer.setAttribute(groupName, key, n5Reader.getAttribute(groupName, key, clazz));
-			} catch (final IOException e) {
-				e.printStackTrace(System.err);
+
+			if (datasetAttributeKeys.contains(key)) {
+				System.out.println("    skipping dataset attribute " + key + " : " + clazz);
+			} else {
+				System.out.println("    " + key + " : " + clazz);
+
+				try {
+					n5Writer.setAttribute(groupName, key, n5Reader.getAttribute(groupName, key, clazz));
+				} catch (final IOException e) {
+					e.printStackTrace(System.err);
+				}
 			}
 		});
 	}
 
 	protected <T extends NativeType<T>> void copyGroup(final String groupName) throws IOException, InterruptedException, ExecutionException {
 
-		System.out.println("Copy group " + groupName);
+		System.out.println(groupName);
 
 		n5Writer.createGroup(groupName);
 		copyAttributes(groupName);

commit 95932caeda9eafd807d03a6d65cf47ba3e9c7fd4

fix bug introduced in d6e7d3afc19e6a2425cda8002b21afddcff0ccc9 when copying from HDF5, dataset attributes cannot be read from groups

src/main/java/org/saalfeldlab/Copy.java
@@ -283,7 +283,8 @@ public class Copy {
 		System.out.println("  attributes:");
 
 		final Map<String, Class<?>> attributes = n5Reader.listAttributes(groupName);
-		final DatasetAttributes datasetAttributes = n5Reader.getDatasetAttributes(groupName);
+
+		final DatasetAttributes datasetAttributes = n5Reader.datasetExists(groupName) ? n5Reader.getDatasetAttributes(groupName) : null;
 		final Set<String> datasetAttributeKeys = datasetAttributes == null ? new HashSet<>() : datasetAttributes.asMap().keySet();
 
 		attributes.forEach((key, clazz) -> {