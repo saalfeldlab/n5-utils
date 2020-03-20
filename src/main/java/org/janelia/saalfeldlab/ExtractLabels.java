7802f7683c79e45644ae037b2b4a6d698efee5ad
Igor Pisarev <pisarevi@janelia.hhmi.org>
2020-03-20 13:05:43 -0400

    save mask crops as unsigned byte data

--- src/main/java/org/janelia/saalfeldlab/ExtractLabels.java
+++ src/main/java/org/janelia/saalfeldlab/ExtractLabels.java
@@ -8,7 +8,7 @@ import net.imglib2.realtransform.RealViews;
 import net.imglib2.realtransform.Scale3D;
 import net.imglib2.type.NativeType;
 import net.imglib2.type.numeric.RealType;
-import net.imglib2.type.numeric.integer.ByteType;
+import net.imglib2.type.numeric.integer.UnsignedByteType;
 import net.imglib2.util.Intervals;
 import net.imglib2.view.Views;
 import org.janelia.saalfeldlab.n5.*;
@@ -67,7 +67,7 @@ public class ExtractLabels implements Callable<Void> {
             Arrays.setAll(upscaledCropMax, d -> upscaledCropMax[d] - 1);
             final Interval upscaledCropInterval = Intervals.smallestContainingInterval(new FinalRealInterval(upscaledCropMin, upscaledCropMax));
             final RandomAccessibleInterval<T> upscaledCrop = Views.interval(upscaledImg, upscaledCropInterval);
-            final RandomAccessibleInterval<ByteType> upscaledCropMask = Converters.convert(upscaledCrop, (in, out) -> out.set(in.getRealDouble() >= threshold ? (byte)1 : 0), new ByteType());
+            final RandomAccessibleInterval<UnsignedByteType> upscaledCropMask = Converters.convert(upscaledCrop, (in, out) -> out.set(in.getRealDouble() >= threshold ? (byte)1 : 0), new UnsignedByteType());
 
             final int[] outBlockSize = blockSizeOptional.orElse(n5Reader.getDatasetAttributes(dataset).getBlockSize());
             final String outputDatasetPath = Paths.get("volumes/labels", dataset).toString();
